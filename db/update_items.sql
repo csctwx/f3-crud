e3a7cc4c2be3d0f846d7a460cd932ca7aa0c4b2a
csctwx <csctwx@msn.com>
2017-09-16 22:54:51 -0400

    update db items

---
+++ db/update_items.sql
@@ -0,0 +1,6 @@
+UPDATE `items` SET `code`=CONCAT(`brand_prefix`,`code`) WHERE `code` RLIKE '^[^A-Z]';
+UPDATE `items` SET `brand_code`=CONCAT(`brand_code`,"A") WHERE `brand_code`="BDDIORAM";
+UPDATE `items` INNER JOIN `brands` ON items.brand_code = brands.code SET `brand_prefix`=brands.prefix;
+
+SELECT * FROM `items` WHERE `code` LIKE "BDDISP%";
+SELECT * FROM `items` LEFT JOIN `brands` ON items.brand_code = brands.code WHERE brands.code IS NULL OR brands.code = '';
